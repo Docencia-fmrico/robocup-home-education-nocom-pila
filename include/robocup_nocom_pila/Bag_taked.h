5053fae90bf2da908eaa2266d98f508f454f7c27
abito2 <victorbameba@gmail.com>
2022-03-31 14:11:10 +0200

    behavior tree, nodes, Cmake, package, config

---
+++ include/robocup_nocom_pila/Bag_taked.h
@@ -0,0 +1,62 @@
+// Copyright 2022 Intelligent Robotics Lab
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef ROBOCUP_NOCOM_PILA_BAG_TAKED_H
+#define ROBOCUP_NOCOM_PILA_BAG_TAKED_H
+
+#include "ros/ros.h"
+
+#include "behaviortree_cpp_v3/behavior_tree.h"
+#include "behaviortree_cpp_v3/bt_factory.h"
+#include "geometry_msgs/Twist.h"
+#include <sensor_msgs/Image.h>
+#include "std_msgs/Float64.h"
+#include "std_msgs/Int64.h"
+
+#include <string>
+
+namespace robocup_nocom_pila
+{
+
+class Bag_taked : public BT::ActionNodeBase
+{
+public:
+    explicit Bag_taked(const std::string& name/*, const BT::NodeConfiguration& config*/);
+
+    void halt();
+
+    BT::NodeStatus tick();
+
+   /*static BT::PortsList providedPorts()
+    {
+        return { BT::InputPort<std::string>("point")};
+    }*/
+
+private:
+    /*const float ADVANCE_SPEED = 0.1;
+    const float TURNING_SPEED = 0.35;
+
+    ros::NodeHandle n_;
+    ros::Publisher vel_pub_;
+    ros::Subscriber dist_point_person;
+    ros::Subscriber px_point_person;
+
+    float dist;
+    int point;*/
+    int counter_;
+};
+
+}  // namespace robocup_nocom_pila
+
+#endif  // ROBOCUP_NOCOM_PILA_BAG_TAKED_H
